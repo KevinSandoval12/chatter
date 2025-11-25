3f209251a188c9fbe5724632a08c5d3ec02bb517
Auberon López <auberon.lopez@greenriver.edu>
2025-11-25 09:53:27 -0800

    method skeletons for client and server

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,6 +1,16 @@
+import java.io.IOException;
+
 public class ChatClient {
 
     public static void main(String[] args) {
+        
+    }
+
+    public static void readFromFile(String filename) throws IOException{
+
+    }
+
+    public static void readFromSocket(String host, String port) throws IOException {
         
     }
 }
---
+++ src/OutServer.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class OutServer {
+    public static void main(String[] args) {
+
+    }
+
+    public void writeToSocket(int port) throws IOException {
+        
+    }
+}
